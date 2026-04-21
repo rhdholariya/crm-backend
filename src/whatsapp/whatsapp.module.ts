b0fde665b0ebc8604710309cc40b67577e865ace
rhdholariya <rahuldholariya001@gmail.com>
2026-04-21 12:08:52 +0530

    changes

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -3,18 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { WhatsAppService } from './whatsapp.service';
 import { WhatsAppController } from './whatsapp.controller';
 import { WhatsAppGateway } from './whatsapp.gateway';
-import { WhatsAppMetaService } from './whatsapp-meta.service';
-import { WhatsAppMetaController } from './whatsapp-meta.controller';
-import { WhatsAppConfig } from './entities/whatsapp-config.entity';
-import { WhatsAppTemplate } from './entities/whatsapp-template.entity';
-import { WhatsAppMessage } from './entities/whatsapp-message.entity';
+// import { WhatsAppMetaService } from './whatsapp-meta.service';
+// import { WhatsAppMetaController } from './whatsapp-meta.controller';
+// import { WhatsAppConfig } from './entities/whatsapp-config.entity';
+// import { WhatsAppTemplate } from './entities/whatsapp-template.entity';
+// import { WhatsAppMessage } from './entities/whatsapp-message.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([WhatsAppConfig, WhatsAppTemplate, WhatsAppMessage]),
-  ],
-  controllers: [WhatsAppController, WhatsAppMetaController],
-  providers: [WhatsAppService, WhatsAppGateway, WhatsAppMetaService],
-  exports: [WhatsAppService, WhatsAppMetaService],
+  imports: [TypeOrmModule.forFeature()],
+  controllers: [WhatsAppController],
+  providers: [WhatsAppService, WhatsAppGateway],
+  exports: [WhatsAppService],
 })
 export class WhatsAppModule {}
